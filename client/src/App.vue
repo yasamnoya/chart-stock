92abb434f5f1b19d23ba18cc2ede9568f4f5daff
J.N. Yiunn <yasamnono@gmail.com>
2021-12-05 20:04:35 +0800

    frontend: set up chart.js

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <div id="app">Homepage</div>
+  <div id="app">
+    Homepage
+    <div>
+      <stock :dateList="dateList" :stockData="stockData"></stock>
+    </div>
+  </div>
 </template>
 
 <script>
 import io from 'socket.io-client';
+import Stock from './components/Stock.vue';
 
 export default {
+  components: {
+    Stock,
+  },
   data: () => ({
     socket: io(process.env.VUE_APP_BACKEND_URL),
+    dateList: ['January', 'February', 'March', 'April', 'May', 'June'],
+    stockData: [
+      {
+        label: '0050',
+        backgroundColor: 'rgb(255, 0, 132)',
+        borderColor: 'rgb(255, 0, 132)',
+        data: [1, 11, 6, 3, 21, 31, 46],
+      },
+      {
+        label: '0056',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
   }),
   created() {
     this.socket.on('connect', () => {
---
+++ client/src/components/Stock.vue
@@ -0,0 +1,27 @@
+<template>
+  <canvas id="myChart"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  props: ['stockData', 'dateList'],
+  data: () => ({
+    chart: null,
+  }),
+  mounted() {
+    const config = {
+      type: 'line',
+      data: {
+        labels: this.dateList,
+        datasets: this.stockData,
+      },
+      options: {},
+    };
+    this.chart = new Chart(document.getElementById('myChart'), config);
+  },
+};
+</script>
+
+<style></style>
